e24386a02b1ef851db5490c0c81204c9bddf23fa
Saleh-Elhadidy <33662513+Saleh-Elhadidy@users.noreply.github.com>
2019-06-07 09:45:22 +0200

    Update and rename test.py to LocPredictor.py
    Commented the file and added description

--- De-Anonymization/LocPredictor.py
+++ De-Anonymization/LocPredictor.py
@@ -30,6 +30,7 @@ except:
 dataset = pd.read_csv('../Locations.csv')
 chosen = []
 i = 0
+# Normalizing the friends record
 def normalize(dataset):
     dataNorm=((dataset-dataset.min())/(dataset.max()-dataset.min()))*1
     dataNorm["Slot"]=dataset["Slot"]
@@ -39,13 +40,13 @@ def normalize(dataset):
     dataNorm["fav_loc"]=dataset["fav_loc"]
     dataNorm["id"]=dataset["id"]
     return dataNorm
-
+# One hot encoding of any required column
 def encode_and_bind(original_dataframe, feature_to_encode):
     dummies = pd.get_dummies(original_dataframe[[feature_to_encode]])
     res = pd.concat([original_dataframe, dummies], axis=1)
     res = res.drop([feature_to_encode], axis=1)
     return(res) 
-
+# removing the columns to be predicted
 data = dataset.drop(columns=['Room'])
 #data[['friends']] = StandardScaler().fit_transform(data[['friends']])
 x_array = np.array(data['friends'])
@@ -56,7 +57,7 @@ classes = dataset['Room'].nunique()
 encoder = LabelEncoder()
 encoder.fit(labels)
 encoded_Y = encoder.transform(labels)
-# convert integers to dummy variables (i.e. one hot encoded)
+# convert integers to one hot encoded labels
 dummy_y = np_utils.to_categorical(encoded_Y)
 from sklearn.metrics import confusion_matrix, precision_score
 from sklearn.model_selection import train_test_split
@@ -68,6 +69,8 @@ X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.
 count = 0
 indexes = []
 '''
+Data anonymization, 257 is 20% of training we multiply to get 20% increments
+
 while j < 257*3:
 	index = random.randint(0,5000)
 	if (X_train.index == index).any():
@@ -89,16 +92,17 @@ while j < 2000:
         break
     j+=1
 '''
+# One hot encoding
 X_train = encode_and_bind(X_train,'Slot')
 X_train = encode_and_bind(X_train,'Student')
 #NewTestData = pd.read_csv('/NewCSV.csv')
 X_test = encode_and_bind(X_test,'Slot')
 X_test = encode_and_bind(X_test,'Student')
-#X_testNew = X_test.loc[(X_test['Slot_Holiday']==1) |  (X_test['Slot_Sat 1st']==1) |  (X_test['Slot_Sat 2nd']==1) |  (X_test['Slot_Sat 3rd']==1)]
+# Making validation set only have holiday records
+#X_testNew = X_test.loc[(X_test['Slot_Holiday']==1)]
 #for index,row in X_test.iterrows():
-	#print(index)
-	#print(row['Slot'])
-#    if (row['Slot_Holiday'] != 1 and row['Slot_Sat 1st'] !=1 and row['Slot_Sat 2nd'] !=1 and row['Slot_Sat 3rd'] !=1 and   count not in indexes):
+
+#    if (row['Slot_Holiday'] != 1 and count not in indexes):
 		#print(index)
 #        indexes.append(count)
 		#print(new_a)
